23c3cdf2d075057948cc0092b779539714ee05fb
Rohit Khatik <rohitkhatik1128@gmail.com>
2026-08-19 15:45:15 +0530

    Fix repository structure and update objects

--- Basics_02/Objects_04.js
+++ Basics_02/Objects_04.js
@@ -9,11 +9,11 @@ tinder.age = 20;
 // console.log(tinder)
 
 const regularObj = {
-    email : "rohitkhatik@gpt.com",
-    name : {
-        fullname : {
-            firstname : "rohit",
-            lastname : "khatik",
+    email: "rohitkhatik@gpt.com",
+    name: {
+        fullname: {
+            firstname: "rohit",
+            lastname: "khatik",
         }
     }
 }
@@ -24,35 +24,35 @@ const regularObj = {
 // console.log(regularObj);
 
 const obj1 = {
-    1 : "a",
-    2 : "b",
-    3 : "c",
+    1: "a",
+    2: "b",
+    3: "c",
 }
 const obj2 = {
-    4 : "d",
-    5 : "e",
-    6 : "f",
+    4: "d",
+    5: "e",
+    6: "f",
 }
 
 // const obj3 = {obj1,obj2}
 
-const obj3 = Object.assign({},obj1,obj2);
+const obj3 = Object.assign({}, obj1, obj2);
 
 // console.log(obj3);
 
 const arrObj = [
 
     {
-        id : 1,
-        email : "fgdfggggr",
+        id: 1,
+        email: "fgdfggggr",
     },
     {
-        id : 2,
-        email : "heruewyriro",
+        id: 2,
+        email: "heruewyriro",
     },
     {
-        id : 3,
-        email : "iefhsljhvfsldfh",
+        id: 3,
+        email: "iefhsljhvfsldfh",
     }
 ]
 
@@ -71,19 +71,21 @@ const arrObj = [
 
 
 const course = {
-    courseInst : "rohit",
-    courseprice : "999",
-    time : "10hr",
+    courseInst: "rohit",
+    courseprice: "999",
+    time: "10hr",
 }
 
 console.log(course.courseInst); // rohit
 
-const {courseInst} = course; // use the name for multiple time
+const { courseInst } = course; // use the name for multiple time
 
 console.log(courseInst); //rohit
 
-const {courseInst : CI} = course; // replace the name in short form for
+const { courseInst: CI } = course; // replace the name in short form for
 
 console.log(CI); //  rohit
 
 
+///---------------------------------
+///--------------------------------
---
+++ JAVA-SCRIPT/README.md
@@ -0,0 +1,2 @@
+# JAVA-SCRIPT
+Learn java script
